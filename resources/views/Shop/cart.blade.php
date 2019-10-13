99c17806a44c78ccf9192551a3950f46670939fd
EviXen <evixen@live.ru>
2019-10-13 22:05:44 +0300

    Главная страница: карусели товаров

--- resources/views/Shop/cart.blade.php
+++ resources/views/Shop/cart.blade.php
@@ -8,6 +8,55 @@
         <div class="row">
             @if(!ShoppingCart::isEmpty())
                 <div class="row justify-content-center">
+
+                    {{-- Список товаров --}}
+                    <div class="col-sm-7">
+                        @foreach(ShoppingCart::all() as $product)
+                            <div class="row align-items-center" id="cart-item">
+                                <div class="col-sm-3">
+                                    <a href="{{ route('shop.product', [$product->category, $product->slug]) }}">
+                                        <img src="{{ asset('/img/products/' . $product->slug . '.jpg') }}" alt="phone"
+                                             style="max-width: 120px" class="img-thumbnail">
+                                    </a>
+                                </div>
+                                <div class="col-sm-3 product-name">
+                                    <a href="{{ route('shop.product', [$product->category, $product->slug]) }}">
+                                        {{ $product->name }}
+                                    </a>
+                                </div>
+                                <div class="col-sm-2 count">
+                                    count
+                                </div>
+                                <div class="col-sm-2 product-price">
+                                    <span class="price">{{ $product->price}} руб.</span>
+                                </div>
+                                <div class="col-sm-2">
+                                    <form action="{{ route('shop.cart.delete', $product->rawId()) }}" method="post">
+                                        {{ csrf_field() }}
+                                        {{ method_field('DELETE') }}
+                                        <button type="submit" class="btn btn-outline-secondary btn-sm">удалить
+                                        </button>
+                                    </form>
+                                </div>
+                            </div><br><br>
+                        @endforeach
+                        <div class="row justify-content-start align-items-center">
+                            <div class="col-sm-4" id="cart-total">
+                                <h3 class="h3">
+                                    Итого: <span class="price">{{ ShoppingCart::totalPrice() }} руб.</span>
+                                </h3>
+                            </div>
+                            <div class="col-sm-4" id="cart-clean">
+                                <form action="{{ route('shop.cart.clean') }}" method="post">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+                                    <button type="submit" class="btn btn-warning">Очистить корзину</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                    {{-- Форма заказа --}}
                     <div class="col-sm-4" id="cart-form">
                         <form method="POST" action="{{ route('shop.cart.checkout') }}">
                             {{ csrf_field() }}
@@ -66,59 +115,13 @@
                                 </div>
                             </div>
                             <button type="submit" class="btn btn-lg btn-purple" id="cart-form-accept">
-                                Оформить заказ
+                                Оформить и оплатить заказ
                             </button>
                         </form>
                     </div>
-
-                    <div class="col-sm-7">
-                        @foreach(ShoppingCart::all() as $product)
-                            <div class="row align-items-center" id="cart-item">
-                                <div class="col-sm-3">
-                                    <a href="{{ route('shop.product', [$product->category, $product->slug]) }}">
-                                        <img src="{{ asset('/img/products/' . $product->slug . '.jpg') }}" alt="phone"
-                                             style="max-width: 120px" class="img-thumbnail">
-                                    </a>
-                                </div>
-                                <div class="col-sm-3 product-name">
-                                    <a href="{{ route('shop.product', [$product->category, $product->slug]) }}">
-                                        {{ $product->name }}
-                                    </a>
-                                </div>
-                                <div class="col-sm-2 count">
-                                    count
-                                </div>
-                                <div class="col-sm-2 product-price">
-                                    <span class="price">{{ $product->price}} руб.</span>
-                                </div>
-                                <div class="col-sm-2">
-                                    <form action="{{ route('shop.cart.delete', $product->rawId()) }}" method="post">
-                                        {{ csrf_field() }}
-                                        {{ method_field('DELETE') }}
-                                        <button type="submit" class="btn btn-outline-secondary btn-sm">удалить
-                                        </button>
-                                    </form>
-                                </div>
-                            </div><br><br>
-                        @endforeach
-                        <div class="row justify-content-end">
-                            <div class="col-sm-4" id="cart-total">
-                                <h3 class="h3">
-                                    Итого: <span class="price">{{ ShoppingCart::totalPrice() }} руб.</span>
-                                </h3>
-                            </div>
-                            <div class="col-sm-4" id="cart-clean">
-                                <form action="{{ route('shop.cart.clean') }}" method="post">
-                                    {{ csrf_field() }}
-                                    {{ method_field('DELETE') }}
-                                    <button type="submit" class="btn btn-warning">Очистить корзину</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             @else
-                <div class="col-md-4" id="cart-empty"><h2>Товаров в корзине нет</h2></div>
+                <div class="col-sm-4" id="cart-empty"><h2>Товаров в корзине нет</h2></div>
             @endif
         </div>
     </div>
